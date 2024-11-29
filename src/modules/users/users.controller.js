30e1f86f30ee5b2b5aedfdd6cea499604c0f868b
minbaby01 <ongbeo111@gmail.com>
2024-11-29 21:54:20 +0700

    #3 - update

--- src/modules/users/users.controller.js
+++ src/modules/users/users.controller.js
@@ -1,47 +1,73 @@
 import HttpStatus from "http-status-codes";
 import usersService from "#src/modules/users/users.service";
 
-export const create = (req, res, next) => {
-  const data = usersService.create();
-  return res.json({
-    statusCode: HttpStatus.CREATED,
-    message: "Create",
-    data,
-  });
+export const create = async (req, res, next) => {
+  try {
+    const data = await usersService.create(req.body);
+    return res.json({
+      statusCode: HttpStatus.CREATED,
+      message: "Create",
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
 };
 
-export const findAll = (req, res, next) => {
-  const data = usersService.findAll();
-  return res.json({
-    statusCode: HttpStatus.OK,
-    message: "Find all",
-    data,
-  });
+export const findAll = async (req, res, next) => {
+  try {
+    const data = await usersService.findAll(req.query);
+    return res.json({
+      statusCode: HttpStatus.OK,
+      message: "Find all",
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      statusCode: HttpStatus.OK,
+      message: "Error",
+      data,
+    });
+  }
 };
 
-export const findOne = (req, res, next) => {
-  const data = usersService.findOne();
-  return res.json({
-    statusCode: HttpStatus.OK,
-    message: "Find one",
-    data,
-  });
+export const findOne = async (req, res, next) => {
+  try {
+    const data = await usersService.findOne(req.params.identify);
+    return res.json({
+      statusCode: HttpStatus.OK,
+      message: "Find one",
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export const update = (req, res, next) => {
-  const data = usersService.update();
-  return res.json({
-    statusCode: HttpStatus.OK,
-    message: "Update",
-    data: "Update",
-  });
+export const update = async (req, res, next) => {
+  try {    
+    const data = await usersService.update(req.params.identify, req.body);
+    return res.json({
+      statusCode: HttpStatus.OK,
+      message: "Update",
+      data: data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export const remove = (req, res, next) => {
-  const data = usersService.remove();
-  return res.json({
-    statusCode: HttpStatus.OK,
-    message: "Remove",
-    data,
-  });
+export const remove = async (req, res, next) => {
+  try {
+    const data = await usersService.remove(req.params.identify);
+    return res.json({
+      statusCode: HttpStatus.OK,
+      message: "Remove",
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
--- src/modules/users/users.service.js
+++ src/modules/users/users.service.js
@@ -2,22 +2,106 @@ import { User } from "#src/modules/users/schemas/user.schema";
 
 export default { create, findAll, findOne, update, remove };
 
-function create() {
-  return "hello";
+async function create(data) {
+  const { avatar = "", name, phone, birthday, gender } = data;
+
+  const newUser = await User.create({
+    avatar: avatar,
+    name: name,
+    phone: phone,
+    birthday: birthday,
+    gender: gender
+  })
+  return newUser;
 }
 
-function findAll() {
-  return "hello";
+async function findAll(data) {
+  let { keyword, sortBy = "name-atoz", status, itemPerPage = 10, page = 1 } = data;
+
+  let filters = {};
+
+  if (keyword) {
+    const regEx = new RegExp(keyword, 'i');
+    filters = {
+      $or: [
+        { name: regEx },
+        { email: regEx },
+      ]
+    }
+  }
+
+  if (status) {
+    filters.status = { $in: status }
+  }
+
+  let sort = {};
+
+  switch (sortBy) {
+    case "name-atoz":
+      sort.name = 1;
+      break;
+    case "name-ztoa":
+      sort.name = -1;
+      break;
+    default:
+      sort.name = 1;
+      break;
+  }
+  const totalItems = await User.countDocuments(filters);
+  const totalPages = Math.ceil(totalItems / itemPerPage);
+
+  if (page < 0) {
+    page = 1;
+  } else if (page > totalPages) {
+    page = totalPages;
+  }
+
+  const offSet = (page - 1) * itemPerPage;
+
+  const users = await User.find(filters).skip(offSet).limit(itemPerPage).sort(sort);
+
+  return {
+    users,
+    pagination: {
+      offSet,
+      itemPerPage,
+      currentPage: page,
+      totalPages,
+      totalItems,
+      isNext: page < totalPages,
+      isPrevious: page > 1 ,
+      isFirst: page > 1 && page <= totalPages,
+      isLast: page >= 1 && page < totalPages 
+    }
+  }
 }
 
-function findOne() {
-  return "hello";
+async function findOne(id) {
+  const user = await User.findById(id);
+
+  if (!user) {
+    throw new Error();
+  }
+
+  return user;
 }
 
-function update() {
-  return "hello";
+async function update(id, data) {
+  const { avatar, name, phone, birthday, gender } = data;
+  const user = await User.findByIdAndUpdate(id, { avatar, name, phone, birthday, gender }, { new: true });
+
+  if (!user) {
+    throw new Error();
+  }
+  return user;
 }
 
-function remove() {
-  return "hello";
+async function remove(id) {
+  const user = await User.findByIdAndDelete(id);
+
+  if (!user) {
+    throw new Error();
+  }
+
+  return "Deleted";
 }
