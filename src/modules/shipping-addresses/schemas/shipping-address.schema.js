2028f5ab22bb95e1774a8fd306d4aa0abb79625a
hiusnguyen201 <hiusnguyen201@gmail.com>
2025-01-03 19:29:00 +0700

    :zap: add schema shipping address

---
+++ src/modules/shipping-addresses/schemas/shipping-address.schema.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const SHIPPING_ADDRESS_MODEL = "shipping_addresses";
+
+const shippingAddressSchema = new Schema(
+  {
+    address: {
+      type: String,
+      length: 255,
+      required: true,
+    },
+    street: {
+      type: String,
+      length: 50,
+      required: true,
+    },
+    city: {
+      type: String,
+      length: 50,
+      required: true,
+    },
+    ward: {
+      type: String,
+      length: 50,
+      required: true,
+    },
+    district: {
+      type: String,
+      length: 50,
+      required: true,
+    },
+    isDefault: {
+      type: Boolean,
+      required: true,
+    },
+
+    // Foreign key
+    customer: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+    _id: true,
+    id: false,
+    collection: SHIPPING_ADDRESS_MODEL,
+  }
+);
+
+const ShippingAddressModel = mongoose.model(
+  "ShippingAddress",
+  shippingAddressSchema
+);
+export { ShippingAddressModel };
