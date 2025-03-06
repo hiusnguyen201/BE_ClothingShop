32d21de7b1d3a00a68408d2d254ad68c57e42ff8
buithaivuong <thaivuong02102002@gmail.com>
2025-03-06 23:03:29 +0700

    #59 fix generateOtp

--- src/utils/string.util.js
+++ src/utils/string.util.js
@@ -1,9 +1,11 @@
 import { REGEX_PATTERNS } from "#src/core/constant";
 import slugifyLib from "slugify";
 
-export const generateOtp = () => {
-  const otp = Math.floor(100000 + Math.random() * 900000).toString();
-  return otp;
+export const generateOtp = (length = 6) => {
+  const otp = Math.floor(
+    Math.pow(10, length - 1) + Math.random() * 9 * Math.pow(10, length - 1)
+  );
+  return otp.toString();
 };
 
 export const randomStr = (length) => {
@@ -13,9 +15,7 @@ export const randomStr = (length) => {
   const charactersLength = characters.length;
   let counter = 0;
   while (counter < length) {
-    result += characters.charAt(
-      Math.floor(Math.random() * charactersLength)
-    );
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
     counter += 1;
   }
   return result;
